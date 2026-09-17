8d8a59e39a72e1177c623ff0eb6eaf184c6d4c44
rorm-dev <107421609+rorm-dev@users.noreply.github.com>
2026-09-17 23:20:53 +0700

    Add monitoring 2

--- src/index-backend.js
+++ src/index-backend.js
@@ -1,4 +1,7 @@
 // config/env.js
+import path from 'path';
+import fs from 'fs';
+import dotenv from 'dotenv';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
@@ -6,16 +9,30 @@ import cors from 'cors'
 import bodyParser from 'body-parser';
 // Determine which .env file to load
 console.log(`Current NODE_ENV: ${process.env.NODE_ENV}`);
+const envFile = `.env.${process.env.NODE_ENV || 'dev'}`;
+const envPath = path.resolve(process.cwd(), envFile);
+
+dotenv.config({ path: envPath });
+
+// Fallback to .env if environment-specific file doesn't exist
+if (!fs.existsSync(envPath)) {
+    dotenv.config();
+}
 
 import express from 'express';
 import router from './routes/index.js';
 const app = express();
+import connectDB from './config/database.js';
+import { version } from 'os';
 import { register, httpRequestDuration } from './metrics.js';
 
+connectDB()
 app.use(helmet());
 app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10kb' }));
 app.use(bodyParser.json({ limit: '10kb' }));
+// app.use(express.json());
+
 
 app.use(cookieParser());
 
@@ -44,7 +61,11 @@ app.get('/health', (req, res) => {
     });
 });
 
+// app.use(hpp({
+//     whitelist: ['filter', 'fields'], // Allow intentional arrays for these params
+// }));
 app.use('/api', router)
+
 
 app.use((err, req, res, next) => {
     res.status(500).json({
